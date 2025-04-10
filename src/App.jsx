1fdf0b0d442f0de84ca0280d8e7b056cb9bc1d5c
Agustin Cordero <agustincorderoacc@gmail.com>
2025-04-10 19:56:03 -0300

    button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import tituloMain from "./componentes/tituloMain/tituloMain"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+//JSX: EXTENSION DE JS. JavaScript Syntax eXtension.
+//caracteristicas:
+//1) toda etiqueta de apertura tiene que tener un cierre. <img/>
+//2) todo componente tiene que tener 1 return
+//3) siempre tiene que devolver 1 elemento
+//4) denbtro del componente, antes del return, puedo escribir cualquier logica de js
+
+//Componentes:
+//Conjunto de elementos que cumplen una funcion especifica en la interfaz del usuario.
+//Ventajas de trabajar con componentes:
+/* 
+-Reutilizamos codigo
+-favorece la separacion de repsonsabilidades
+-El codigo se vuelve mas facil de entender
+-Mejora el rendicmiento de la aplicacion
+
+
+Caracteristicas:
+-Renderiza un unico elemento
+-Pueden recibir props(propiedades de los componentes)
+
+¿que son esas props?
+las props son objetos que contienen datos que un componente necesita para rederizarse correctamente.
+y estas se envian de forma unidireccional de un componente padre a un componente hijo.
+
+*/
+
+const App = () => {
+
+  function saludo() {
+    console.log("hola")
+  }
+
+  saludo()
+
+  let colorFondo= {backgroundColor: "green"}
+
+
+  const celular = {
+    marca: "Samsung",
+    precio: 200.000
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* PREENTREGA */}
+      <NavBar/>
+      <ItemListContainer/>
+
+      {/* <div>App</div>
+      <h2>hola mundo</h2>
+      <h3 style={colorFondo}>clase 2</h3>
+      <h3 style={{color:"blue"}}>cualqueircosa</h3> */}
+
+      {/* <TituloPrincipal saludo = "Hola Comision" producto={celular}/>
+      <Boton texto="Comprar"/>
+      <Boton texto="Eliminar"/> */}
+
+      {/* <ItemCount stock={15}/>
+      <ItemCount stock={5}/>
+      <ItemCount stock={20}/>
+       */}
     </>
   )
 }
